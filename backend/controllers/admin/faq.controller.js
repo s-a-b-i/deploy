37adcb07c1a38e08550260bb7ff541add12beb4a
Muhammad Haroon <theharoonsocial@gmail.com>
2025-02-03 00:50:19 +0500

    fix bugs in delete category middleware

--- backend/controllers/admin/faq.controller.js
+++ backend/controllers/admin/faq.controller.js
@@ -36,12 +36,14 @@ export const deleteCategory = async (req, res) => {
     const { adminId, categoryId } = req.body;
     await checkAdmin(adminId);
 
-    const category = await Category.findByIdAndDelete(categoryId);
+    const category = await Category.findOneAndDelete({ _id: categoryId });
     if (!category) {
       return res.status(404).json({ error: 'Category not found.' });
     }
 
-    return res.status(200).json({ message: 'Category deleted successfully', category });
+    await FAQ.deleteMany({ category: categoryId });
+
+    return res.status(200).json({ message: 'Category and related FAQs deleted successfully', category });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
