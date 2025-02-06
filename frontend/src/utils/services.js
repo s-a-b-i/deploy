e9ff8c6a326a6772547f59014f0ca477b2ba632b
Sohaib <21ntucs1360@gmail.com>
2025-02-06 10:51:09 +0500

    latest commmit

--- frontend/src/utils/services.js
+++ frontend/src/utils/services.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = 'https://deploy-sder.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
